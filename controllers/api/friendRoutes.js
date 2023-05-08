9783411a477cec27bd8d9863f42a3961d947bde1
eschindev <118208710+eschindev@users.noreply.github.com>
2023-05-08 14:15:34 -0700

    created API routes for friends and family

---
+++ controllers/api/friendRoutes.js
@@ -0,0 +1,60 @@
+const router = require("express").Router();
+const { Friend } = require("../../models");
+
+router.post("/", async (req, res) => {
+  try {
+    const friendData = await Friend.create(req.body);
+
+    if (!friendData) {
+      return res
+        .status(400)
+        .json({ message: "Failed to create friend. Please try again. " });
+    }
+
+    res.status(200).json(friendData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.put("/:id", async (req, res) => {
+  try {
+    const friendData = await Friend.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!friendData) {
+      return res
+        .status(400)
+        .json({ message: "Failed to update friend. Please try again." });
+    }
+
+    res.status(200).json(friendData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.delete("/:id", async (req, res) => {
+  try {
+    const friendData = await Friend.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!friendData) {
+      return res
+        .status(400)
+        .json({ message: "Failed to delete friend. Please try again." });
+    }
+
+    res.status(200).json(friendData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
