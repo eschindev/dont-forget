097079f0e1677139177a994de1b7ad073cfb8f26
eschindev <118208710+eschindev@users.noreply.github.com>
2023-05-09 12:12:36 -0700

    added photo upload functionality to client-side scripts and friend post/put endpoints

--- public/new-friend.js
+++ public/new-friend.js
@@ -9,6 +9,20 @@ const newFriendFormHandler = async (event) => {
   const phone = document.querySelector("#phone_input").value.trim();
   const email = document.querySelector("#email_input").value.trim();
   const address = document.querySelector("#address_input").value.trim();
+  const photoInput = document.querySelector("#photo_input");
+
+  let photo;
+
+  if (photoInput.files[0]) {
+    try {
+      photo = await readFileAsBase64(photoInput.files[0]);
+    } catch (err) {
+      console.log(
+        "Failed to upload photo. Please check file format and try again."
+      );
+      return;
+    }
+  }
 
   if (name) {
     const response = await fetch(`/api/friend/`, {
@@ -20,6 +34,7 @@ const newFriendFormHandler = async (event) => {
         phone,
         email,
         address,
+        photo,
       }),
       headers: { "Content-Type": "application/json" },
     });
@@ -33,6 +48,17 @@ const newFriendFormHandler = async (event) => {
     }
   }
 };
+
+function readFileAsBase64(file) {
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = () => {
+      resolve(reader.result.split(",")[1]);
+    };
+    reader.onerror = (error) => reject(error);
+  });
+}
 
 document
   .querySelector(".add-friend-form")
