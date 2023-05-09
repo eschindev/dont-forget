13351532e7e0df1bb3a4cc18a509e49e79362658
eschindev <118208710+eschindev@users.noreply.github.com>
2023-05-09 13:36:39 -0700

    added dropzone.js code to new-friend and edit-friend client-side scripts

--- public/edit-friend.js
+++ public/edit-friend.js
@@ -1,3 +1,15 @@
+const Dropzone = require("dropzone");
+
+let photo;
+
+const myDropzone = new Dropzone(".dropzone", {
+  autoProcessQueue: false,
+  paramName: "photo",
+  acceptedFiles: "image/*",
+  maxFiles: 1,
+  createImageThumbnails: false,
+});
+
 const editFriendFormHandler = async (event) => {
   event.preventDefault();
 
@@ -9,34 +21,25 @@ const editFriendFormHandler = async (event) => {
   const phone = document.querySelector("#phone_input").value.trim();
   const email = document.querySelector("#email_input").value.trim();
   const address = document.querySelector("#address_input").value.trim();
-  const friendId = window.location.pathname.split("/")[2];
-  const photoInput = document.querySelector("#photo_input");
+  const friendId = parseInt(window.location.pathname.split("/")[2]);
 
-  let photo;
+  const data = {
+    name,
+    birthday,
+    significant_other,
+    phone,
+    email,
+    address,
+  };
 
-  if (photoInput.files[0]) {
-    try {
-      photo = await readFileAsBase64(photoInput.files[0]);
-    } catch (err) {
-      console.log(
-        "Failed to upload photo. Please check file format and try again."
-      );
-      return;
-    }
+  if (photo) {
+    data.photo = photo;
   }
 
   if (Number.isInteger(friendId)) {
     const response = await fetch(`/api/friend/${friendId}`, {
       method: "PUT",
-      body: JSON.stringify({
-        name,
-        birthday,
-        significant_other,
-        phone,
-        email,
-        address,
-        photo,
-      }),
+      body: JSON.stringify(data),
       headers: { "Content-Type": "application/json" },
     });
 
@@ -48,16 +51,17 @@ const editFriendFormHandler = async (event) => {
   }
 };
 
-function readFileAsBase64(file) {
-  return new Promise((resolve, reject) => {
-    const reader = new FileReader();
-    reader.readAsDataURL(file);
-    reader.onload = () => {
-      resolve(reader.result.split(",")[1]);
-    };
-    reader.onerror = (error) => reject(error);
-  });
-}
+myDropzone.on("addedfile", (file) => {
+  document.querySelector("#submit-button").disabled = true;
+
+  const reader = new FileReader();
+  reader.onload = (event) => {
+    photo = event.target.result.split(",")[1];
+
+    document.querySelector("#submit-button").disabled = false;
+  };
+  reader.readAsDataURL(file);
+});
 
 document
   .querySelector(".edit-friend-form")
