988a04a82af763362c1ce0b7955f71cb47f0de8c
Laurie Fish <iamlauriefish@gmail.com>
2023-05-08 20:43:08 -0700

    added login, signin, main and friends pages

---
+++ signup.js
@@ -0,0 +1,28 @@
+const questions = [
+    { question: 'Enter Your First Name' },
+    { question: 'Enter Your Last Name' },
+    { question: 'Enter Your Email', pattern: /\S+@\S+\.\S+/ },
+    { question: 'Create A Password', type: 'password' }
+];
+
+// Transition Times
+const shakeTime = 100; // Shake Transition Time
+const switchTime = 200; // Transition Between Questions
+
+// Init Position At First Question
+let position = 0;
+
+// Init DOM Elements
+const formBox = document.querySelector('#form-box');
+const nextBtn = document.querySelector('#next-btn');
+const prevBtn = document.querySelector('#prev-btn');
+const inputGroup = document.querySelector('#input-group');
+const inputField = document.querySelector('#input-field');
+const inputLabel = document.querySelector('#input-label');
+const inputProgress = document.querySelector('#input-progress');
+const progress = document.querySelector('#progress-bar');
+
+// EVENTS
+
+// Get Question On DOM Load
+document.addEventListener('DOMContentLoaded', getQuestion);
